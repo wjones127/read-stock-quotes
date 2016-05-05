048ba65aaf0c294af4f94086dada13d95971f067
Will Jones <thefreethinker127@gmail.com>
2016-05-05 11:20:40 -0700

    Tried using vector instead of switch

--- src/converter.c
+++ src/converter.c
@@ -9,7 +9,7 @@
 
 void convert_all(unsigned nlines, char *lines[], quote_t nums[])
 {
-    //printf("Number of lines: %d\n", nlines);
+    printf("Number of lines: %d\n", nlines);
     const char* zero_char = "0";
     const int zero = (int)zero_char[0];
 
@@ -24,9 +24,13 @@ void convert_all(unsigned nlines, char *lines[], quote_t nums[])
     */
 
     // adjustment constants for different sizes
+    
     const quote_t fix_4d = 1111 * zero;
     const quote_t fix_3d = 111 * zero;
     const quote_t fix_5d = 11111 * zero;
+    
+    //const quote_t fixes[6] = { 0, 0, 0, 111 * zero, 1111 * zero, 11111 * zero };
+    
 
     // Tried to use OpenMP to do the conversion in parallel,
     // but it actually was slower.
@@ -46,26 +50,31 @@ void convert_all(unsigned nlines, char *lines[], quote_t nums[])
         sum = (int)lines[i][0];
         sum = (10 * sum) + ((int)lines[i][1]);
         sum = (10 * sum) + ((int)lines[i][2]);
-        if (lines[i][3] != 0) {
+        if (lines[i][3] != 0)
+            {
     sum = (10 * sum) + ((int)lines[i][3]);
-            j = 4;
-            if (lines[i][4] != 0) {
+    j = 4;
+    if (lines[i][4] != 0) {
     sum = (10 * sum) + ((int)lines[i][4]);
-                j = 5;
-            }
-        }
+    j = 5;
+}
+}
         else {
-            j = 3;
-        }
+    j = 3;
+}
         
 
         //printf("stopped at j = %d\n", j);
         // If it's a 4-digit number (most common case)
+        
         switch (j) {
         case 4: sum = sum - fix_4d; break;
         case 3: sum = sum - fix_3d; break;
         case 5: sum = sum - fix_5d; break;
         }
+        
+        //sum = sum - fixes[j];
+        
         /*        if (j == 4) sum = sum - fix_4d;
         else if (j == 3) sum = sum - fix_3d;
         else sum = sum - fix_5d;*/
