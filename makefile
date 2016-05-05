ac0fdf2352c437a63db20d754b8321d8c9bef5a9
Will Jones <thefreethinker127@gmail.com>
2016-05-04 21:17:10 -0700

    Use integer representation to convert strings

--- makefile
+++ makefile
@@ -3,5 +3,5 @@ CFLAGS = -O3 -std=c99 -march=native -Werror -Wall -Wextra -pedantic
 build/test: src/converter.c
 	gcc $(CFLAGS) -o build/test src/converter.c src/driver.c
 
-test:
-	build/test quotes.txt 1000
+test: build/test
+	build/test quotes.txt 1
--- src/converter.c
+++ src/converter.c
@@ -5,10 +5,30 @@
 #include "converter.h"
 
 #include <stdlib.h>
+#include <stdio.h>
 
 void convert_all(unsigned nlines, char *lines[], quote_t nums[])
 {
-  for (unsigned i = 0; i < nlines; i++) {
-    nums[i] = atoi(lines[i]);
-  }
+    printf("Number of lines: %d\n", nlines);
+    const char* zero_char = "0";
+    const int zero = (int)zero_char[0];
+
+    for (unsigned i = 0; i < 10; i++) {
+        printf("%s\n", lines[i]);
+        
+        printf("Are these equal? %c, %d, %d \n",
+               lines[i][0], (int)lines[i][0], (int)lines[i][0] - zero);
+
+    }
+
+    
+    for (unsigned i = 0; i < nlines; i++) {
+        //nums[i] = atoi(lines[i]);
+        quote_t sum = 0;
+        for (unsigned j = 0; lines[i][j] != 0; j++) {
+            sum = (10 * sum) + ((int)lines[i][j] - zero);
+        }
+        //        printf("%"PRIu32"\n", sum);
+        nums[i] = sum;
+    }
 }
