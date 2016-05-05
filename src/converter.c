e5cf469f1ce24632c550f891ea6907256e2fd2c8
Will Jones <thefreethinker127@gmail.com>
2016-05-05 14:23:43 -0700

    Add expectation to number length conditionals
    Because we expect mostly 4 digit number, I added statements to tell
    the compiler to expect those kinds of numbers in the if statements
    that determine how many digits to consider. This gave a slight performance
    boost.

--- src/converter.c
+++ src/converter.c
@@ -9,7 +9,7 @@
 
 void convert_all(unsigned nlines, char *lines[], quote_t nums[])
 {
-    printf("Number of lines: %d\n", nlines);
+    //printf("Number of lines: %d\n", nlines);
     const char* zero_char = "0";
     const int zero = (int)zero_char[0];
 
@@ -29,50 +29,47 @@ void convert_all(unsigned nlines, char *lines[], quote_t nums[])
     const quote_t fix_3d = 111 * zero;
     const quote_t fix_5d = 11111 * zero;
     
-    //const quote_t fixes[6] = { 0, 0, 0, 111 * zero, 1111 * zero, 11111 * zero };
+    //const quote_t fixes[3] = {111 * zero, 1111 * zero, 11111 * zero };
+    //const uint8_t pow_ten[4] = {1, 1, 10, 100};
     
 
-    // Tried to use OpenMP to do the conversion in parallel,
-    // but it actually was slower.
     unsigned i;
-    //#pragma omp parallel for private(i)
+
     for (i = 0; i < nlines; i++) {
         //nums[i] = atoi(lines[i]);
         quote_t sum = 0;
-        unsigned j;
+        unsigned j;                                        
 
-        /*
-          for (j = 0; lines[i][j] != 0; j++) {
-          sum = (10 * sum) + ((int)lines[i][j]);
-          }
-        */
         // Handle first three digits
-        sum = (int)lines[i][0];
-        sum = (10 * sum) + ((int)lines[i][1]);
-        sum = (10 * sum) + ((int)lines[i][2]);
-        if (lines[i][3] != 0)
+        sum = (100 * (int)lines[i][0]) + (10 * (int)lines[i][1]) + (int)lines[i][2];
+
+        // Handle last two digits
+        /*sum = (sum * pow_ten[j+1]) + (four_digits * pow_ten[j] * d4) +
+          (five_digits * d5) - fixes[j];*/
+
+        if (__builtin_expect((lines[i][3] != 0), 1))
             {
-    sum = (10 * sum) + ((int)lines[i][3]);
-    j = 4;
-    if (lines[i][4] != 0) {
-    sum = (10 * sum) + ((int)lines[i][4]);
-    j = 5;
-}
-}
+                sum = (10 * sum) + (int)lines[i][3];
+                j = 4;
+                if (__builtin_expect((lines[i][4] != 0), 0)) {
+                    sum = (10 * sum) + (int)lines[i][4];
+                    j = 5;
+                }
+            }
         else {
-    j = 3;
-}
-        
+            j = 3;
+        }
+            
 
         //printf("stopped at j = %d\n", j);
         // If it's a 4-digit number (most common case)
-        
+
         switch (j) {
         case 4: sum = sum - fix_4d; break;
         case 3: sum = sum - fix_3d; break;
         case 5: sum = sum - fix_5d; break;
         }
-        
+
         //sum = sum - fixes[j];
         
         /*        if (j == 4) sum = sum - fix_4d;
