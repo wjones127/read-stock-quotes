48e79cc1e63bfb117581b0d6c3c396d248d6a86f
Will Jones <thefreethinker127@gmail.com>
2016-05-09 10:37:07 -0700

    Uncomment optimized version

--- src/converter.c
+++ src/converter.c
@@ -6,12 +6,12 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-/*
+
 static quote_t add_first_three(char *lines[], uint32_t i)
 {
     return (100 * (int)lines[i][0]) + (10 * (int)lines[i][1]) + (int)lines[i][2];
 }
-*/
+
 void convert_all(unsigned nlines, char *lines[], quote_t nums[])
 {
     //printf("Number of lines: %d\n", nlines);
@@ -23,30 +23,26 @@ void convert_all(unsigned nlines, char *lines[], quote_t nums[])
       speed, we precompute the total value to subtract for 3, 4, and 5 digit
       numbers.
     */
-    /*
+    
     const char* zero_char = "0";
     const int zero = (int)zero_char[0];
     // adjustment constants for different sizes
     const quote_t fix_3d = 111 * zero;
     const quote_t fix_4d = 1111 * zero;
     const quote_t fix_5d = 11111 * zero;
-    */
+    
 
     /*
     for (unsigned i = 0; i < 10; i++) {
     printf("%s\n", lines[i]);
-        
-    printf("Are these equal? %c, %d, %d \n",
-    lines[i][0], (int)lines[i][0], (int)lines[i][0] - zero);
-
     }
     */
 
     unsigned i;
 
     for (i = 0; i < nlines; i++) {
-        nums[i] = atoi(lines[i]);
-        /*
+        //nums[i] = atoi(lines[i]);
+        
         quote_t sum = 0;
 
         // Handle first three digits
@@ -70,6 +66,6 @@ void convert_all(unsigned nlines, char *lines[], quote_t nums[])
         }
 
         //printf("%"PRIu32"\n", sum);
-        nums[i] = sum;*/
+        nums[i] = sum;
     }
 }
